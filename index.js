90ec5d4c70639201f6451faa4faa8f737797d011
Quinn <quinn@indra.ai>
2022-11-03 14:23:52 -0700

    adv to space

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const SPACE = new Deva({
     ***************/
     maps(packet) {
       const {params} = packet.q.meta;
-      const map = `img:${params[1].m}/map/${packet.q.text}`;
+      const map = `img:${params[0].m}/map/${packet.q.text}`;
 
       return new Promise((resolve, reject) => {
         this.question(`#feecting parse:${params[0]}:${params[1]} ${map}`).then(parsed => {
